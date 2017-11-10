56fd548044511d3f493bc5029ba17dfde65e0dec
Dylan Fox <dylan-fox@users.noreply.github.com>
2017-11-10 15:49:05 -0800

    Selection scene added, UI scene removed

---
+++ VRTK-master/Assets/Custom Scripts/SelectionScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectionScript : MonoBehaviour {
+
+	public OVRInput.Controller controller;
+
+	private float indexTriggerState = 0;
+	private float oldIndexTriggerState = 0;
+	
+	// Update is called once per frame
+	void Update () {
+		oldIndexTriggerState = indexTriggerState;
+		indexTriggerState = OVRInput.Get (OVRInput.Axis1D.PrimaryIndexTrigger, controller);
+
+		print ("Index State: " + indexTriggerState);
+
+	}
+}
