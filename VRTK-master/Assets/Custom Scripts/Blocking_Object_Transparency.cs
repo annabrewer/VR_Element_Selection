959b9e479711bd29da0f7498364ac95eca25ad19
Dylan Fox <dylan-fox@users.noreply.github.com>
2017-11-10 17:52:15 -0800

    Updated Selection & Blocking Object Transparency
    Selection currently lets you click on an object repeatedly to have it cycle through transparency states. Blocking Object Transparency makes things trasnparent if they're inbetween your controller and your headset.

---
+++ VRTK-master/Assets/Custom Scripts/Blocking_Object_Transparency.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blocking_Object_Transparency : MonoBehaviour {
+
+	public GameObject OVRCameraRig;
+	public GameObject leftHandAnchor;
+	public GameObject rightHandAnchor;
+
+	private float rightDist;
+	private float leftDist;
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 cameraPos = OVRCameraRig.transform.localPosition;
+		Vector3 rPos = rightHandAnchor.transform.localPosition;
+		Vector3 lPos = leftHandAnchor.transform.localPosition;
+
+		RaycastHit hit;
+		Ray camToRight = new Ray (cameraPos, rPos);
+		Ray camToLeft = new Ray (cameraPos, lPos);
+
+		rightDist = (rPos - cameraPos).magnitude;
+		leftDist = (lPos - cameraPos).magnitude;
+
+		if (Physics.Raycast (camToRight, out hit, rightDist)) {
+			if (hit.collider.tag == "object") {
+				//print ("Object hit.");
+				//GameObject other = hit.collider;
+				Material otherMat = hit.collider.GetComponent<Renderer>().material;
+				ChangeAlpha (otherMat, 0.5f);
+				//Now how to change it back when you cease contact?
+			}
+		}
+	}
+
+	private static void ChangeAlpha(Material mat, float alphaValue) {
+		Color oldColor = mat.color;
+		Color newColor = new Color(oldColor.r, oldColor.g, oldColor.b, alphaValue);
+		mat.SetColor("_Color", newColor);
+	}
+}
--- VRTK-master/Assets/Custom Scripts/SelectionScript.cs
+++ VRTK-master/Assets/Custom Scripts/SelectionScript.cs
@@ -6,15 +6,49 @@ public class SelectionScript : MonoBehaviour {
 
 	public OVRInput.Controller controller;
 
+
 	private float indexTriggerState = 0;
 	private float oldIndexTriggerState = 0;
-	
+	private Material otherMat;
+
+
 	// Update is called once per frame
 	void Update () {
 		oldIndexTriggerState = indexTriggerState;
 		indexTriggerState = OVRInput.Get (OVRInput.Axis1D.PrimaryIndexTrigger, controller);
 
-		print ("Index State: " + indexTriggerState);
 
+	}
+
+	void OnTriggerStay (Collider other) {
+		if (other.CompareTag ("object")) {
+			//print ("Object encountered.");
+
+			if (indexTriggerState >= 0.9f && oldIndexTriggerState < 0.9f) {
+				print ("Object selected.");
+				Material otherMat = other.GetComponent<Renderer>().material;
+				//Color newColor = Color.white;
+				//Color newColor = new Vector4(otherMat.color.r, otherMat.color.g, otherMat.color.b, otherMat.color.a / 2);
+				//otherMat.color = newColor;
+				//otherMat.color.a = 0.5f;
+				//other.renderer.material.color = otherMat.ChangeAlpha(0.5);
+				if (otherMat.color.a >= 0.1)
+					ChangeAlpha (otherMat, otherMat.color.a / 2);
+				else
+					ChangeAlpha (otherMat, 1f);
+
+				print ("otherMat.color.r: " + otherMat.color.r);
+				print ("otherMat.color.g: " + otherMat.color.g);
+				print ("otherMat.color.b: " + otherMat.color.b);
+				print ("otherMat.color.a: " + otherMat.color.a);
+			}
+
+		}
+	}
+
+	private static void ChangeAlpha(Material mat, float alphaValue) {
+		Color oldColor = mat.color;
+		Color newColor = new Color(oldColor.r, oldColor.g, oldColor.b, alphaValue);
+		mat.SetColor("_Color", newColor);
 	}
 }
