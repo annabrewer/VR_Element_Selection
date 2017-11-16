225ac2073c108e6b82de6af1e9a98840300a59dd
Dylan Fox <dylan-fox@users.noreply.github.com>
2017-11-16 14:39:40 -0800

    Element Filter Display
    Added an icon that floats next to each controller indicating what the current element filter setting is.

---
+++ VRTK-master/Assets/Custom Scripts/ElementFilterDisplay.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElementFilterDisplay : MonoBehaviour {
+
+	private int selectionMode;
+	private GameObject vertexObj;
+	private GameObject edgeObj;
+	private GameObject faceObj;
+	private GameObject objectObj;
+	private GameObject allObj;
+
+
+
+	// Use this for initialization
+	void Start () {
+		vertexObj = transform.Find ("Vertex").gameObject;
+		edgeObj = transform.Find ("Edge").gameObject;
+		faceObj = transform.Find ("Face").gameObject;
+		objectObj = transform.Find ("Object").gameObject;
+		allObj = transform.Find ("All").gameObject;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		selectionMode = MenuModeSelection.selectionMode;
+
+		//Enable and disable child components based on selection mode.
+		if (selectionMode == 0) {
+			vertexObj.active = true;
+			edgeObj.active = false;
+			faceObj.active = false;
+			objectObj.active = false;
+			allObj.active = false;
+		} 
+
+		if (selectionMode == 1) {
+			vertexObj.active = false;
+			edgeObj.active = true;
+			faceObj.active = false;
+			objectObj.active = false;
+			allObj.active = false;
+		} 
+
+		if (selectionMode == 2) {
+			vertexObj.active = false;
+			edgeObj.active = false;
+			faceObj.active = true;
+			objectObj.active = false;
+			allObj.active = false;
+		} 
+
+		if (selectionMode == 3) {
+			vertexObj.active = false;
+			edgeObj.active = false;
+			faceObj.active = false;
+			objectObj.active = true;
+			allObj.active = false;
+		} 
+
+		if (selectionMode == 4) {
+			vertexObj.active = false;
+			edgeObj.active = false;
+			faceObj.active = false;
+			objectObj.active = false;
+			allObj.active = true;
+		} 
+
+
+	}
+}
+
